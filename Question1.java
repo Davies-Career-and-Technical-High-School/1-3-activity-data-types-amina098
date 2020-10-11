48b57cd27c2796ea0efc64cf1d0511f006d55f30
Amina Nadeem <amina.nadeem@daviestech.org>
2020-10-11 14:57:34 +0000

    done

--- Question1.java
+++ Question1.java
@@ -1,9 +1,12 @@
-
+import java.util.Scanner;
 
 class Question1 {
-    public static void main(String[] args) {
-
-        /* Write your code here */
-
+    public static void main(String[] args) { 
+    Scanner scan = new Scanner (System.in);
+    System.out.println("Type in two decimal numbers");
+    double num1 = scan.nextDouble();
+    double num2 = scan.nextDouble();
+    System.out.println(num2 + "  " + num1);
+        
     }
 }
--- Question2.java
+++ Question2.java
@@ -1,9 +1,14 @@
-
+import java.util.Scanner;
 
 class Question2 {
     public static void main(String[] args) {
-
-        /* Write your code here */
+    Scanner scan = new Scanner (System.in);
+    System.out.println("Hi There, What is your name");
+    String name = scan.nextLine();
+    System.out.println( " Hi " + name  + " How old are you ");
+    int age = scan.nextInt();
+    System.out.println(name + " is " + age + " years old");
+       
 
     }
 }
